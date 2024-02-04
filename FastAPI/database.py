8c94e161851f27817a52ec59eb7e54bd946e128a
kitou88C39 <kanfutrooper@icloud.com>
2024-02-04 20:25:25 +0900

    Create React and FastAPI folders

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -6,4 +6,6 @@ URL_DATABASE = 'sqlite:///./finance.db'
 
 engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
 
-SessionLoacal = sessionmaker
+SessionLoacal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
