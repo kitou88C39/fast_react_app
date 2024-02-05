df0bf1a525ac256c4ad70645e984005a64baf27b
kitou88C39 <kanfutrooper@icloud.com>
2024-02-06 08:59:57 +0900

    Create main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -51,3 +51,5 @@ async def create_transaction(transaction: TransactionBase, db: db_dependency):
 
 @app.get("/transactions", response_model=List[TransactionModel])
 async def read_transactions(db: db_dependency, skip: int = 0, limit: int = 100):
+    transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
+    return transactions
