49ecd22ae900de2f5320672050e9f4d0972cca30
kitou88C39 <kanfutrooper@icloud.com>
2024-02-05 13:42:33 +0900

    Create main.py

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -7,3 +7,12 @@ import models
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    add_origins=origins,
+)
