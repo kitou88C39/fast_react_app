09fdff349d0fcecb619ef7b039263ebab2e393cb
kitou88C39 <kanfutrooper@icloud.com>
2024-02-06 17:00:12 +0900

    Edit App.js

--- React/finance_app/src/App.js
+++ React/finance_app/src/App.js
@@ -124,6 +124,28 @@ const App = () => {
             Submit
           </button>
         </form>
+        <table className='table table-striped table-bordered table-hover'>
+          <thead>
+            <tr>
+              <th>Amount</th>
+              <th>Category</th>
+              <th>Description</th>
+              <th>Income</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.description}</td>
+                <td>{transaction.is_income ? 'Yes' : 'No'}</td>
+                <td>{transaction.data}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
