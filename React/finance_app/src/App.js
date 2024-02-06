a7f87ce8676ae08979b094b5147d14c2bf7ff608
kitou88C39 <kanfutrooper@icloud.com>
2024-02-06 11:51:57 +0900

    Edit App.js

--- React/finance_app/src/App.js
+++ React/finance_app/src/App.js
@@ -1,4 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import api from './api';
 
+const App = () => {
+  const [transaction, setTransaction] = useState([]);
+  const [formData, setFormData] = useState({
+    amount: '',
+    category: '',
+    description: '',
+    is_income: false,
+    date: '',
+  });
+};
+
 export default App;
